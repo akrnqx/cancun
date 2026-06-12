52090de34fad538eb81455a699842b97305a0ca2
akrnqx <akrnqx@gmail.com>
2026-06-12 23:11:28 +0200

    not much

--- src/arch/x86/page.zig
+++ src/arch/x86/page.zig
@@ -1,3 +1,6 @@
+pub const Phys = u64;
+pub const Virt = u64;
+
 const TableLevel = enum {
     lv4,
     lv3,
@@ -23,9 +26,6 @@ fn EntryBase(tl: TableLevel) type {
         restart: bool = false,
         phys: u51,
         xd: bool = false,
-
-        pub const Phys = u64;
-        pub const Virt = u64;
 
         pub inline fn address(self: Self) Phys {
             return @as(u64, @intCast(self.phys)) << 12;
@@ -66,3 +66,23 @@ const Lv4Entry = EntryBase(.lv4);
 const Lv3Entry = EntryBase(.lv3);
 const Lv2Entry = EntryBase(.lv2);
 const Lv1Entry = EntryBase(.lv1);
+
+const page_mask_4k: u64 = 0xFFF;
+const num_table_entries: usize = 512;
+
+fn getTable(T: type, addr: Phys) []T {
+    const ptr: [*]T = @ptrFromInt(addr & ~page_mask_4k);
+    return ptr[0..num_table_entries];
+}
+fn getLv4Table(cr3: Phys) []Lv4Entry {
+    return getTable(Lv4Entry, cr3);
+}
+fn getLv3Table(lv3_paddr: Phys) []Lv3Entry {
+    return getTable(Lv3Entry, lv3_paddr);
+}
+fn getLv2Table(lv2_paddr: Phys) []Lv2Entry {
+    return getTable(Lv2Entry, lv2_paddr);
+}
+fn getLv1Table(lv1_paddr: Phys) []Lv1Entry {
+    return getTable(Lv1Entry, lv1_paddr);
+}
