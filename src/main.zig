9a576e774df65d144955893220b6c9b23282885b
akrnqx <akrnqx@gmail.com>
2026-07-06 11:20:39 +0200

    refractor

--- src/main.zig
+++ src/main.zig
@@ -4,23 +4,8 @@ const elf = std.elf;
 const llog = @import("log.zig");
 const arch = @import("arch.zig");
 
-pub fn StatusResponse(comptime T: type) type {
-    return struct {
-        status: efi.Status,
-        response: T,
-
-        pub fn getResponse(self: @This()) T {
-            return self.response;
-        }
-
-        pub fn getStatus(self: @This()) efi.Status {
-            return self.status;
-        }
-    };
-}
-
 pub fn main() efi.Status {
-    const con_out = getConout() catch {
+    var con_out = getConout() catch {
         return .unsupported;
     };
 
@@ -28,24 +13,22 @@ pub fn main() efi.Status {
         return .aborted;
     };
 
-    const res = init();
-
-    if (res.getStatus() != .success) {
-        return res.getStatus();
+    var boot_service: efi.tables.BootServices = undefined;
+    const res = init(&boot_service);
+    if (res != .success) {
+        return res;
     }
-
-    const boot_service = res.getResponse() orelse return .aborted;
 
     arch.c_arch.map4kTo(
         0xFFFF_FFFF_DEAD_0000,
         0x10_0000,
         .read_write,
-        boot_service,
+        &boot_service,
     ) catch {
         return .aborted;
     };
 
-    arch.c_arch.setLv4Writable(boot_service) catch {
+    arch.c_arch.setLv4Writable(&boot_service) catch {
         return .load_error;
     };
 
@@ -66,54 +49,49 @@ fn init_con_out(con_out: *efi.protocol.SimpleTextOutput) !void {
     llog.init(con_out);
 }
 
-pub fn init() StatusResponse(?*efi.tables.BootServices) {
-    const boot_service = efi.system_table.boot_services orelse {
+pub fn init(bsp: *efi.tables.BootServices) efi.Status {
+    var boot_service = bsp;
+    boot_service = efi.system_table.boot_services orelse {
         llog.log(.err, "failed to get boot services", .{});
-        return .{
-            .response = null,
-            .status = .aborted,
-        };
+        return .aborted;
     };
 
     const fs_optional = boot_service.locateProtocol(efi.protocol.SimpleFileSystem, null) catch |err| {
         llog.log(.err, "locate protocol failed with {}", .{err});
-        return .{ .response = boot_service, .status = .aborted };
+        return .aborted;
     };
 
     var fs: *efi.protocol.SimpleFileSystem = fs_optional orelse {
         llog.log(.err, "locate protocol is null", .{});
-        return .{ .response = boot_service, .status = .aborted };
+        return .aborted;
     };
 
     const root_dir: *efi.protocol.File = fs.openVolume() catch |err| {
         llog.log(.err, "could not open volume {}", .{err});
-        return .{ .response = boot_service, .status = .aborted };
+        return .aborted;
     };
 
     const kernel = openFile(root_dir, "CAB.ELF") catch return .aborted;
     const header_size: usize = @sizeOf(elf.Elf64_Ehdr);
     var header_buffer: []align(8) u8 = boot_service.allocatePool(.loader_data, header_size) catch {
         llog.log(.err, "Failed to allocate memory for kernel ELF header.", .{});
-        return .{ .response = boot_service, .status = .aborted };
+        return .aborted;
     };
 
     const b_read = kernel.read(header_buffer) catch {
         llog.log(.err, "Failed to read kernel ELF header.", .{});
-        return .{ .response = boot_service, .status = .aborted };
+        return .aborted;
     };
 
     var fixed_reader = std.Io.Reader.fixed(header_buffer[0..b_read]);
     const elf_header = elf.Header.read(&fixed_reader) catch |err| {
         llog.log(.err, "Failed to parse kernel ELF header: {}", .{err});
-        return .{ .response = boot_service, .status = .aborted };
+        return .aborted;
     };
 
     _ = elf_header;
 
-    return .{
-        .response = boot_service,
-        .status = .success,
-    };
+    return .success;
 }
 
 fn openFile(
